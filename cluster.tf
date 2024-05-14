aa0a23f7ddecb76ed25283f2489498afa41dfc57
Rosario Di Somma <rosario.disomma@dreamhost.com>
2024-05-14 14:31:05 +0000

    Add kube api server sec group

--- cluster.tf
+++ cluster.tf
@@ -10,8 +10,14 @@ terraform {
 
 provider "openstack" {}
 
-resource "openstack_networking_secgroup_v2" "instance_comms" {
+resource "openstack_networking_secgroup_v2" "kubelet_sec_group" {
   name = "Kubelet API"
   description = "Security group for allowing Kubelet to communicate with the API server"
+  delete_default_rules = false
+}
+
+resource "openstack_networking_secgroup_v2" "kube_api_server_sec_group" {
+  name = "Kubernetes API server"
+  description = "Security group for allowing Kube API server to talk with Kubelet"
   delete_default_rules = false
 }
