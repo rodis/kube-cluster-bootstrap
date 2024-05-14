54586ad3dcc01390ffa2fe0d0b2b9301486718e4
Rosario Di Somma <rosario.disomma@dreamhost.com>
2024-05-14 14:45:17 +0000

    Open kube api server port to the outside

--- cluster.tf
+++ cluster.tf
@@ -21,3 +21,14 @@ resource "openstack_networking_secgroup_v2" "kube_api_server_sec_group" {
   description = "Security group for allowing Kube API server to talk with Kubelet"
   delete_default_rules = false
 }
+
+# Allow tcp on port 6443 for IPv4 within security group (API server)
+resource "openstack_networking_secgroup_rule_v2" "rule_k8s_tcp_6443_ipv4" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+  port_range_min = 6443
+  port_range_max = 6443
+  remote_group_id = openstack_networking_secgroup_v2.kube_api_server_sec_group.id
+  security_group_id = openstack_networking_secgroup_v2.kube_api_server_sec_group.id
+}
