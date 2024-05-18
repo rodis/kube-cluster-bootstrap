1a57122e4a3851903dcff54ef2e0c8f0b12eeab4
Rosario Di Somma <rosario.disomma@dreamhost.com>
2024-05-18 11:16:37 +0000

    Fix error

--- master.tf
+++ master.tf
@@ -2,7 +2,7 @@ resource "openstack_compute_instance_v2" "master" {
   name = "k8s-${var.PROJECT_NAME}-master"
   flavor_name = "gp1.warpspeed"
   key_pair = "dh_machines"
-  security_groups = [ "default", openstack_networking_secgroup_v2.kube_api_server_sec_group.name, openstack_networking_secgroup_v2.etcd_sec_group ]
+  security_groups = [ "default", openstack_networking_secgroup_v2.kube_api_server_sec_group.name, openstack_networking_secgroup_v2.etcd_sec_group.name ]
 
   block_device {
     uuid                  = "2b2c61c6-324c-47f4-88c1-9ae8a978ddfd"
