4b84250981c8a3cdea005fcce553fa1373ed2835
Rosario Di Somma <rosario.disomma@dreamhost.com>
2024-05-25 16:04:40 +0000

    Fix syntax error

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "node" {
-  count = INTERNAL_AZ == "west" ? var.NODES : var.NODES + 1
+  count = var.INTERNAL_AZ == "west" ? var.NODES : var.NODES + 1
   name = "k8s-${var.INTERNAL_AZ}-node-${count.index+1}"
   flavor_name = var.flavor_name
   key_pair = var.key_pair
