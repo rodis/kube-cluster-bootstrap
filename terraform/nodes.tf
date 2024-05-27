b23def205c30c300aa75c9221c0103038717c0d4
Rosario Di Somma <rosario.disomma@dreamhost.com>
2024-05-27 14:26:33 +0000

    Fix nodes counting logic

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "node" {
-  count = local.is_control_tenant ? var.NODES : var.NODES - 2
+  count = local.is_control_tenant ? var.NODES -2 : var.NODES
   name = "k8s-${var.INTERNAL_AZ}-node-${count.index+1}"
   flavor_name = var.flavor_name
   key_pair = var.key_pair
