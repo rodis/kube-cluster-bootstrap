daefc5ad9a8910369f99d6a56837d860992bcc79
Rosario Di Somma <rosario.disomma@dreamhost.com>
2024-05-27 14:07:43 +0000

    Add nginx controller sec group

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,3 @@
 locals {
- create_master = var.INTERNAL_AZ == "west" || var.INTERNAL_AZ == "north" ? true : false
+ is_control_tenant = var.INTERNAL_AZ == "west" || var.INTERNAL_AZ == "north" ? true : false
 }
--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "node" {
-  count = local.create_master ? var.NODES : var.NODES + 1
+  count = local.is_control_tenant ? var.NODES : var.NODES + 1
   name = "k8s-${var.INTERNAL_AZ}-node-${count.index+1}"
   flavor_name = var.flavor_name
   key_pair = var.key_pair
