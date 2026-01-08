6ee810db65661df9ce284f68b878cd29dc86a4fa
Rosario Di Somma <rosario.disomma@dreamhost.com>
2026-01-08 17:48:23 +0000

    Fix typo

--- terraform/kubernetes-infra/master.tf
+++ terraform/kubernetes-infra/master.tf
@@ -7,7 +7,7 @@ resource "openstack_compute_instance_v2" "master" {
     "default",
     openstack_networking_secgroup_v2.kube_api_server_sec_group.name,
     openstack_networking_secgroup_v2.kubelet_sec_group.name,
-    openstack_networking_secgroup_v2.nginx_controller_sec_group.name
+    openstack_networking_secgroup_v2.nginx_controller_sec_group.name,
     openstack_networking_secgroup_v2.etcd_sec_group.name
   ]
 
