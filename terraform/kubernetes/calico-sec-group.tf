3483ad20412935ac512d6dacc44e6c05119d92d9
Rosario Di Somma <rosario.disomma@dreamhost.com>
2026-02-22 16:50:26 +0000

    Attach calico rule to the correct group

--- terraform/kubernetes/calico-sec-group.tf
+++ terraform/kubernetes/calico-sec-group.tf
@@ -11,5 +11,5 @@ resource "openstack_networking_secgroup_rule_v2" "rule_calico_vxlan_udp_4789_ipv
   protocol  = "udp"
   port_range_min = 4789
   port_range_max = 4789
-  security_group_id = openstack_networking_secgroup_v2.kube_api_server_sec_group.id
+  security_group_id = openstack_networking_secgroup_v2.calico_node_sec_group.id
 }
